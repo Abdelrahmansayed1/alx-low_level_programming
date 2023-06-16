87c7f7e02f3ddb20952b07fd20be69ee894b8187
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-16 21:08:01 +0300

    first

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,15 +12,16 @@ int main(void)
 {
 	char ch = 'a';
 
-        while (ch <= 'z')
-        {
-		if(ch == 'q' || ch == 'e')
-			ch++;
-                putchar(ch);
-		ch++;
-        }
-        putchar('\n');
 
-        return (0);
+       while (ch <= 'z')
+       {
+	       if(ch == 'q' || ch == 'e')
+		       ch++;
+               putchar(ch);
+	       ch++
+       }
+       putchar('\n');
+
+       return (0);
 }
 
