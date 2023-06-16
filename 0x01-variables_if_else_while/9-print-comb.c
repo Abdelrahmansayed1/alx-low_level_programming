bbb4ccfe0f0a4ead3f9bc64316bea3919f34862a
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-16 22:04:09 +0300

    first

---
+++ 0x01-variables_if_else_while/9-print-comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : print
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int ch = 48;
+	char CH = 'a';
+
+	/* prints a-z*/
+	while (ch <= 57)
+	{
+		putchar(ch);
+		if (ch != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		ch++;
+	}
+
+	return (0);
+}
+
