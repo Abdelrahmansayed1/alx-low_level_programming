6b26608a5802cf61b5f0a7a05e6ff66730425bd3
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-16 21:22:41 +0300

    first

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description :a
+ *
+ * Return : Always 0 (success)
+*/
+
+int main(void)
+{
+	for (int i = 0; i < 11; i++)
+		printf("%i",i);
+	printf("\n");
+
+	return (0);
+}
