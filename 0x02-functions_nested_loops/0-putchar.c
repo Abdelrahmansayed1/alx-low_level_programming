14d2dfc328e69d3103548ad6f3dfbc7c15cc650c
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-20 16:21:33 +0300

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,22 @@
  *
  * Return: 0 (success)
 */
+#define MAXSTRING 80
+
 int main(void)
 {
 	int i ;
 	char word[] = "_putchar";
 
-	for(i = 0; i < strlen(word); i++)
-		putchar(word[i]);
-	
+	for(i = 0; i < MAXSTRING; i++)
+	{
+		if (word[i] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+			putchar(word[i]);
+	}
 	return 0;
 }
