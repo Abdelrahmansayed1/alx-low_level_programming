89a735c66d843137d2d9536ba00d1ba08a6740bb
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-20 19:16:43 +0300

    first

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,21 +14,21 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 			 if (n == 98)
-                        printf("98\n");
-                else
-                        printf("%i, ", n);
-                n--;
+                        	printf("98\n");
+                	else
+                        	printf("%i, ", n);
+                	n--;
 		}
 	else
 	{
 		while (n <= 98)
-        {
+        	{
                 if (n == 98)
                         printf("98\n");
                 else
                         printf("%i, ", n);
                 n++;
-        }
+        	}
 
 	}
 }
