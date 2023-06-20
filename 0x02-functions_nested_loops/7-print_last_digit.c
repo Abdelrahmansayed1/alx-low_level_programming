7c245e798985213b93acbc0136cae11314f269ff
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-20 17:40:27 +0300

    first

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute of number
+ *
+ * @n: checks input of function
+ *
+ * Return: returns absolute of `n`
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
