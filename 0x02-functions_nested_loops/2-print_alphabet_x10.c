dc44685457b9ea82912010329d7bc2c487db2d4c
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-20 16:41:02 +0300

    first

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print form a to z
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i;
+	for (i = 0; i < 11; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
