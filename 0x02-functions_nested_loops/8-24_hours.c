66ab9e21ac0bb123acf1fb403836c910ba890225
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-20 17:58:38 +0300

    first

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns last digit of number
+ *
+ * @n: checks input of function
+ *
+ * Return: returns alast digit of `n`
+*/
+
+void jack_bauer(void)
+{
+	int min, hr;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min =0; min <= 59; min++)
+		{
+			_putchar(hr / 10);
+			_putchar(hr % 10);
+			_putchar(':');
+			_putchar(min / 10);
+			_putchar(min % 10);
+			_putchar('\n');
+		}
+	}
+
+}
