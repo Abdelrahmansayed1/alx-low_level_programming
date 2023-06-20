88bcf3ac18b25fd96ebc5c096eb649bf20d3fc68
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-20 18:43:04 +0300

    first

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,13 +11,17 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
+		printf("%i",0);
 		for ( col = 0; col <= 9; col++)
 		{
+			printf(", ");
 			number = row * col;
-			if (number > 0 && number < 9)
-				printf("%i,  ",number);
-			else if (number >= 9)
-				printf("%i, ",number);
+			if (number < 9)
+				printf(" ");
+			else	
+				printf("%i",number);
+
+			printf("%i",number);
 		}
 		printf("\n");
 	}
