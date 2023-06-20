875fe81b7d31a137e306c42dc3b2b0772aa702fc
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-20 16:28:51 +0300

    first

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: a
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
