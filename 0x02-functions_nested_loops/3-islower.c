2cda3362e56c4bc397e1c8061777924a8f3049a3
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-20 16:56:18 +0300

    first

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,9 @@
 /**
  * _islower - check if alphabet is lower case
  *
+ * @c: checks input of function
+ *
+ * Return: returns 1 if `c` is lowercase otherwise `0` (success)
 */
 
 int _islower(int c)
