dd135ee93e8ccd3955c6b02d73ee1e79bc98e006
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-20 16:50:40 +0300

    first

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print form a to z 10 times
+ *
+*/
+
+int _islower(int c)
+{
+	if (97 <= c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
+
