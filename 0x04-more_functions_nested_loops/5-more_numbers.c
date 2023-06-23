14f027930a520a9756e71dabd01ce41a2201a033
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-23 20:49:03 +0300

    fist

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - return from 0 to 9
+ *
+ * Return: always 0 (success)
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int num;
+
+	for (i = 1; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			num = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				num = j % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
+}
