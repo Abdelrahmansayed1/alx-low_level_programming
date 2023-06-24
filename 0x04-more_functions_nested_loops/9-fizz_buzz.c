989348e1a6ca6d156c4606cd35957b595a1e3e97
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-24 14:58:42 +0300

    fist

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ";
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%i", i);
+	}
+}
