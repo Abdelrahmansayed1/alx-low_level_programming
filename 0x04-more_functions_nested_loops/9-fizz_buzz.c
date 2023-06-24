8fe6645d83ce191d777bc87e22f981c24b68d5ce
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-24 15:09:42 +0300

    fist

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,20 +16,25 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
 			printf("%i ", i);
 		}
+
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
 
 	return (0);
