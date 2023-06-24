d85a4da43fc12b10036eb30618314dae3b8fadcf
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-24 14:39:53 +0300

    fist

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print square of #
+ *
+ * @size: represents length and width of square
+ *
+ * Return: always 0 (success)
+*/
+
+void print_square(int size)
+{
+	if (size == 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+}
