f6e89109a889823e18760cbff5b4be4a8fa20b78
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-26 22:27:47 +0300

    1

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts2 - function that prints a string, followed by a new line
+ *
+ * @str: represents string s
+ *
+ * Return: always 0 (success)
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		printf("%c", s[i]);
+		i = i + 2;
+	}
+}
