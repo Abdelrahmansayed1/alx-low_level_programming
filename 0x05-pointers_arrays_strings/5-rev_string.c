88a914799c76ec90994f41f1994be2dece5e582d
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-26 22:22:09 +0300

    1

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,9 +17,9 @@ void rev_string(char *s)
 		;
 
 	for (i = 0; i < l / 2; i++)
-		{
-			temp = s[i];
-			s[i] = s[l - 1 - i];
-			s[l - 1 - i] = temp;
-		}
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
 }
