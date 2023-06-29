bdf6ac0c4d9b6c629192aec1819d6457cbca02ef
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-29 15:42:02 +0300

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,17 +18,17 @@ char *_strncat(char *dest, char *src, int n)
 
 	i = 0;
 
-	while (dest[i])
+	while (dest[ i ])
 	{
 		i++;
 	}
 	
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	for (j = 0; j < n && src[ j ] != '\0'; j++)
 	{
-		dest[i + j] = src[j];
+		dest[ i + j ] = src[ j ];
 	}
 
-	dest[i + j] = '\0';
+	dest[ i + j ] = '\0';
 
 	return (dest);
 }
