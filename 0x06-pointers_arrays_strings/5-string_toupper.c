89aed6c94ce04378f830ccdb39b03e20e1e12cbf
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-06-29 16:37:45 +0300

    1

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that return uppercase of string.
+ *
+ * @str: represents string
+ *
+ * Return: string upper
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+		str ++;
+	}
+	return (str);
+}
