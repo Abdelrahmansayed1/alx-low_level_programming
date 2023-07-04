27aa79413784a8c733f7b19cf19e608fe910b077
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-07-04 16:14:08 +0300

    first

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_memcpy - function used to copies memory area
+ *
+ * @dest: pointer to string dest
+ *
+ * @src: pointer to copied string
+ *
+ * @n: number of bytes to be copied
+ *
+ * return: pointer to string
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src [i];
+	}
+	return (dest);
+}
