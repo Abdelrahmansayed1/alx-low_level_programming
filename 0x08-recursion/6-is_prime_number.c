5726d8f3e5c9dc9a4190b50b6eec864ba2090ed7
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-07-05 21:30:01 +0300

    first

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Write a function that returns sqyare root of number
+ *
+ * @n: integer
+ *
+ * Return: square root of number
+*/
+int prime(int n, int val);
+int is_prime_number(int n)
+{
+	return int prime(n, 2);
+}
+
+int prime(int n, int val)
+{
+	if (val == n)
+		return (1);
+	else if (n % val == 0)
+	{
+		return (0);
+	}
+	else (n % val != 0)
+		return (prime(n, val + 1);
+}
