ec230fee0e744e290d57baa1734ca8ac1636f837
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-07-05 20:12:13 +0300

    first

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Write a function that returns power of number
+ *
+ * @x: integer
+ *
+ * @y: power
+ *
+ * Return: power of number
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (x);
+	}
+	return (x * _pow_recursion(x, y-1);
+}
