a0b9b5fe637653077e356f322a0fc58fcecacd47
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-07-07 22:23:53 +0300

    2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints program name
+ *
+ * @argc: int
+ *
+ * @argv: list
+ *
+ * return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	int n1, n2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%i", n1 * n2);
+	}
+	return (0);
+}
