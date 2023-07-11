b5b0f7727c20214d5812a0f5489ee63a26b71c38
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-07-11 18:31:03 +0300

    first

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * f*create_array - Write a function that returns array of chars
+ *
+ * @size: integer
+ *
+ * @c : char
+ *
+ * Return: string of chars
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *s = (char*)malloc(size*(sizeof(char));
+	int i;
+
+	if (size == 0 || s == 0)
+		return (0);
+
+	for (i = 0; i < size; i++)
+			s[i] = c;
+	return (s);
+}
