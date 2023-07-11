f112756f5c7053bd2e66eb978d1ddead44c08a36
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-07-11 19:51:37 +0300

    first

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -20,7 +20,7 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (; i < ac; i++, nc ++)
+	for (; i < ac; i++, nc++)
 		nc += strlen(av[i]);
 
 	s = malloc(sizeof(char) * nc + 1);
