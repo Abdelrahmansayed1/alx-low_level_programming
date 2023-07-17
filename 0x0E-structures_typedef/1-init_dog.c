64b4e84fd94be76f64260e1e8a18fcdcab1c476a
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-07-17 20:15:17 +0300

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * init dog - initiates a dog
+ *
+ * @d: the dog
+ *
+ * @name: dog name
+ *
+ * @age: dog age
+ *
+ * @owner: dog owner
+ *
+ * Return: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
