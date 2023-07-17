a64543a016ed0ea9594bf0448154439d77e8e832
abdelrahman-sayed <abdelrahmansayedu@gmail.com>
2023-07-17 19:41:33 +0300

    1

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ *
+ * Description: 'the program's description
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
