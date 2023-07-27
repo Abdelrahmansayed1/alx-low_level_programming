782a980bb4d0aaf18922f2fb9419d8939c8b752f
Abdelrahmansayed1 <abdelrahmansayedu@gmail.com>
2023-07-27 19:50:50 +0300

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - print list length
+ *
+ * @h: list
+ *
+ * Return: length of list
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t l = 0;
+
+	while (h)
+	{
+		l++;
+	}
+	return (l);
+}
