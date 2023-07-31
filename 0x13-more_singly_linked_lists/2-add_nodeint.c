0f93f014fa05ad363b6716e72e38a819cc7cdb4b
Abdelrahmansayed1 <abdelrahmansayedu@gmail.com>
2023-07-31 21:21:04 +0300

    1

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,7 @@
  * Return: the address of the new element, or NULL if it fails
  */
 
-listint_t *add_node(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
