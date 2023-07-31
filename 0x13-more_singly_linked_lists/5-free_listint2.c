0c24d794c8c885ec5469dd2d3ced45490045006c
Abdelrahmansayed1 <abdelrahmansayedu@gmail.com>
2023-07-31 21:41:55 +0300

    1

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,11 +14,11 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (head)
+	while (*head)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = *head->next;
+		free(*head);
+		*head = temp;
 	}
 	*head = NULL;
 }
