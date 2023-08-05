aaaffc4c105bf382aa1364027d29ec4a97859433
Abdelrahmansayed1 <abdelrahmansayedu@gmail.com>
2023-08-05 19:17:28 +0300

    1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+/**
+ * binary_to_unit - function converts binary to int
+ *
+ * *b: string
+ *
+ * Return: number
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int num = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		num = 2 * num + (b[i] - '0');
+	}
+	return (num);
+}
