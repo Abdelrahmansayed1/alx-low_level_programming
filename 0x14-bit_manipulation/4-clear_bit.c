15a1d0817ae53b4565bedd2560969db3d3c79940
Abdelrahmansayed1 <abdelrahmansayedu@gmail.com>
2023-08-05 19:51:25 +0300

    1

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,10 +9,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizefof(n) * 8)
+	if (index >= sizeof(n) * 8)
 		return (-1);
 
 	if(*n & 1L << index)
-		*n ^= 1l << index;
+		*n ^= 1L << index;
 	return (1);
 }
