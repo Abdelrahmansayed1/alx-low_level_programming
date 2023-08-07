e7d8517a37c086d7fb7fca71a3df693a381fd43a
Abdelrahmansayed1 <abdelrahmansayedu@gmail.com>
2023-08-07 22:17:37 +0300

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,9 +20,9 @@ int create_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | Q_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	w = write(fd, text_content, len);
+	w = write(fd, text_content, length);
 
 	if (fd == -1 || w == -1)
 		return (-1);
