01d01ced0c23af6a308b3955ec1daaf40e079a03
Abdelrahmansayed1 <abdelrahmansayedu@gmail.com>
2023-08-08 15:21:52 +0300

    2

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@
 #include <unistd.h>
 
 #define READ_BUF_SIZE 1024
+#define _POSIX_C_SOURCE  200809L
+#define _GNU_SOURCE
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
