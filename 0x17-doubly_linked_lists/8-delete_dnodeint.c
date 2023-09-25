85a50318b665feee32047989751a603a670fd2f9
Abdelrahman Sayed <119951252+Abdelrahmansayed1@users.noreply.github.com>
2023-09-25 16:16:11 +0300

    Create 8-delete_dnodeint.c

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
+
+	h11 = *head;
+
+	if (h11 != NULL)
+		while (h11->prev != NULL)
+			h1 = h11->prev;
+
+	i = 0;
+
+	while (h11 != NULL)
+	{
+		if (i == index)
+		{
+			if (i == 0)
+			{
+				*head = h11->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h11->next;
+
+				if (h11->next != NULL)
+					h11->next->prev = h2;
+			}
+
+			free(h11);
+			return (1);
+		}
+		h2 = h11;
+		h11 = h11->next;
+		i++;
+	}
+
+	return (-1);
+}
