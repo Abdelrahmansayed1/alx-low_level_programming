5ba39ddc8b59678ba5267b50e1e73a134e5164e0
Abdelrahman Sayed <119951252+Abdelrahmansayed1@users.noreply.github.com>
2023-09-25 16:08:17 +0300

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int c = 0;
+
+	if (h == NULL)
+		return (c);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		c++;
+		h = h->next;
+	}
+
+	return (c);
+}
